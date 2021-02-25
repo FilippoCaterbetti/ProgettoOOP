1846f406d9b10aa74076dfa52a9ac09b16196913
leandrofalasca <74785620+leandrofalasca@users.noreply.github.com>
2021-02-25 16:35:34 +0100

    Update NotAllowedPeriodException.java

--- OPENWEATHER/src/main/java/com/project/OPENWEATHER/exception/NotAllowedPeriodException.java
+++ OPENWEATHER/src/main/java/com/project/OPENWEATHER/exception/NotAllowedPeriodException.java
@@ -1,11 +1,12 @@
 package com.project.OPENWEATHER.exception;
 
+import java.lang.Exception;
 
 //contiene eccezione per un periodo non valido
 //periodi ammessi: giornaliero , settimanale, mensile
 
 
-public class NotAllowedPeriodException {
+public class NotAllowedPeriodException extends Exception {
 	String error;
 	
 	public NotAllowedPeriodException(String error) {
