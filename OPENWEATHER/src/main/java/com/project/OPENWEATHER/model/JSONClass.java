dc79dd787545c6339ef4322fd88668b1e713aad0
leandrofalasca <74785620+leandrofalasca@users.noreply.github.com>
2021-02-26 21:56:17 +0100

    interface JSON

---
+++ OPENWEATHER/src/main/java/com/project/OPENWEATHER/model/JSONClass.java
@@ -0,0 +1,10 @@
+package com.project.OPENWEATHER.model;
+
+import org.json.JSONObject;
+
+public interface JSONClass {
+	//le classi che implementeranno quest interfaccia  ottengono n JSONObject 
+	//@return JSONObject contenente i dati 
+	
+	public JSONObject getJson();
+}
