152138ecfaf4b2cc3b63f41c179d4e8911076ee2
leandrofalasca <74785620+leandrofalasca@users.noreply.github.com>
2021-02-26 10:40:01 +0100

    Update Service.java

--- OPENWEATHER/src/main/java/com/project/OPENWEATHER/service/Service.java
+++ OPENWEATHER/src/main/java/com/project/OPENWEATHER/service/Service.java
@@ -1,9 +1,26 @@
 package com.project.OPENWEATHER.service;
 
+
+import java.util.ArrayList;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer.ConditionObject;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.project.OPENWEATHER.exception.InvalidStringException;
+import com.project.OPENWEATHER.exception.NotAllowedPeriodException;
+import com.project.OPENWEATHER.model.City;
+//da finire
 public interface Service {
 	
-	public abstract JSONObject getCityWeather(String city);
-	public abstract City getTempfromApi(String city);
-	public abstract String save(String city) throws invalidException;
-	public String FiveHourInfo(String cityName);
+	public abstract JSONObject getCityApi(String name);
+	public abstract City getTempApi(String name);
+	public abstract String save(String name) throws InvalidStringException;
+	public String FiveHourInfo(String name);
+	public String FiveDaysInfo(String name);
+	public abstract JSONArray getTempAPI(String name);
+
+	public abstract ArrayList<JSONObject> PeriodCity (String name, String period ) throws InvalidStringException, NotAllowedPeriodException;  //inserire vari errori 
+	
+	
 }
