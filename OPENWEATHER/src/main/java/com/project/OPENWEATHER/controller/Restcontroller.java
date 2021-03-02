2a19c38295de15bb7d23d9591e8df2da91574f5a
leandrofalasca <s1098306@studenti.univpm.it>
2021-03-02 19:59:54 +0100

    Update Restcontroller.java

--- OPENWEATHER/src/main/java/com/project/OPENWEATHER/controller/Restcontroller.java
+++ OPENWEATHER/src/main/java/com/project/OPENWEATHER/controller/Restcontroller.java
@@ -11,8 +11,10 @@ import java.lang.Exception;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,6 +25,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.OPENWEATHER.exception.CitynotFoundException;
 import com.project.OPENWEATHER.exception.InvalidStringException;
 import com.project.OPENWEATHER.exception.NotAllowedPeriodException;
 import com.project.OPENWEATHER.model.Temperature;
@@ -37,12 +40,16 @@ public class Restcontroller {
 	Service service;
 	
 	/**
+	 * 
+	 * * Rotta di tipo GET che mostra le temperature 
+	 * per i 5 giorni successivi della città.
+	 * 
 	 * @param name indichiamo la città da cui vogliamo la temperatura.
 	 * @return restituiamo le previsioni della città indicata
 	 * 
 	 */
 	
-	@GetMapping("/temp")
+	@GetMapping(value="/temp")
 	public ResponseEntity<Object> getTemp(@RequestParam String name) {
 		return new ResponseEntity<> (service.getTempApi(name), HttpStatus.OK);
     }
@@ -74,7 +81,7 @@ public class Restcontroller {
      *  }*/
 	
 	//il "period"(giornaliero, settimanale, mensile) indica il periodo di tempo da selezionare
-	@PostMapping("/PeriodCity")
+	@PostMapping(value="/PeriodCity")
 	public ResponseEntity<Object> PeriodCity(@RequestBody String body) throws InvalidStringException, NotAllowedPeriodException{
 		
 		
@@ -100,7 +107,7 @@ public class Restcontroller {
 	 * 
 	 * @param name = città
 	 */
-	@GetMapping("/FiveDaysInfo")
+	@GetMapping(value="/FiveDaysInfo")
 	public ResponseEntity<Object> FiveDaysInfo(@RequestParam String name){
 		
 		return null;
@@ -118,6 +125,90 @@ public class Restcontroller {
 	@GetMapping(value="/FiveHoursInfo")
     public ResponseEntity<Object> FiveHoursInfo(@RequestParam String name) throws IOException {
 			String report5hour = service.FiveHoursInfo(name);
+			
 		return new ResponseEntity<> (report5hour, HttpStatus.OK);
 	}
+	
+	
+	
+	/**
+	 * Rotta di tipo POST che filtra in base al periodo le statistiche sulle temperature della città
+	 * Il JSONObject deve essere di questo tipo per funzionare: 
+	 * 
+	 * {
+     *     "cities": [
+     *        {
+     *          "name": "Ancona"
+     *        },
+     *        {
+     *          "name": "Bologna"  //nel caso fi piu città
+     *        }
+     *      ],
+     *     "period": "giornaliero"
+     *  }
+	 * 
+	 * il "period" può essere solo giornaliero, settimanale, mensile.
+	 * . Le città ammesse sono solo Ancona, Campobasso, Macerata, Roma, San Martino in Pensilis e Tolentino.
+	 * 
+	 * @param body è il JSONObject sopra.
+	 * @return  statistiche delle città con la periodicità indicata.
+	 * 
+	 * @throws InvalidStringException se la stringa è errata/vuota.
+	 * @throws CitynotFoundException se la città non esiste.
+	 * @throws NotAllowedPeriodException se viene inserita un  period errato
+	 * @throws IOException se ci sono errori di input da file.
+	 * 
+	 */
+	
+	@PostMapping(value="/statsHistory")
+    public ResponseEntity<Object> statsHistory(@RequestBody String body) 
+    		throws InvalidStringException, CitynotFoundException, NotAllowedPeriodException, IOException {
+		
+		JSONObject object = new JSONObject(body);
+        JSONArray array = new JSONArray();
+
+        array = object.getJSONArray("cities");
+        
+        ArrayList<String> cities = new ArrayList<String>(array.length());
+        
+        for(int i=0; i<array.length();i++) {
+        	
+            JSONObject obj = new JSONObject();
+            obj = array.getJSONObject(i);
+            cities.add(obj.getString("name"));
+        }
+		
+        String period = object.getString("period");
+        
+        try {
+        	return new ResponseEntity<>(service.HistoryOfTemps(cities,period).toString(),HttpStatus.OK);
+        }
+        
+        catch (CitynotFoundException e) {
+        	
+			return new ResponseEntity<>(e.getError(),HttpStatus.BAD_REQUEST);
+		
+        }
+        catch (InvalidStringException e) {
+        	
+			return new ResponseEntity<>(e.getError(),HttpStatus.BAD_REQUEST);
+		
+        }
+        
+        catch (NotAllowedPeriodException e) {
+        	
+        	return new ResponseEntity<>(e.getError(),HttpStatus.BAD_REQUEST);
+        
+        }
+        
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
