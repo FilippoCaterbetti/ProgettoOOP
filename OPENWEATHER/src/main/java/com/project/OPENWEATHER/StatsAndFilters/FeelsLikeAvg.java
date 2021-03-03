464c60a6676d4042959c66ffe4b1041ef53c98a9
FilippoCaterbetti <s1095055@studenti.univpm.it>
2021-03-03 09:43:15 +0100

    Update FeelsLikeAvg.java

--- OPENWEATHER/src/main/java/com/project/OPENWEATHER/StatsAndFilters/FeelsLikeAvg.java
+++ OPENWEATHER/src/main/java/com/project/OPENWEATHER/StatsAndFilters/FeelsLikeAvg.java
@@ -5,11 +5,15 @@ import java.util.Vector;
 
 import org.json.JSONArray;
 
+import com.project.OPENWEATHER.exception.InvalidStringException;
 import com.project.OPENWEATHER.model.City;
 import com.project.OPENWEATHER.model.Temperature;
 
 public class FeelsLikeAvg implements FiltersStatistics{
-
+	
+	Filters v = new Filters();
+	String value = v.getValue();
+	
 	public double DAY1(Vector <Temperature> dati) {
 		
 		double sommaTemp=0;
@@ -37,37 +41,45 @@ public class FeelsLikeAvg implements FiltersStatistics{
 * 
 * Restituisce la città con il massimo dislivello di temperatura percepita del primo giorno 
 */
-	public City maxFeelsLikeDAY1(Vector<City> toAnalyze){
-				
+	public City maxFeelsLikeOneday(Vector<City> toAnalyze) throws InvalidStringException{
+			
+		if(value.equals("FeelsLike") || value.equals("Fl") || value.equals("percepita")) {
+
 			City giveback = toAnalyze.elementAt(0);
 			for(City c : toAnalyze)
 				if(this.DAY1(c.getTemps()) > this.DAY1(giveback.getTemps()))
 					giveback = c;
 			return giveback;
-		}
+		}	else throw new InvalidStringException (value+" è una stringa errata! Devi inserire una stringa tra FeelLike/Fl/percepita");
+	}
 /**
 * 
 * Restituisce la città con il massimo dislivello di temperatura percepita della settimana
 */
-	public City maxFeelsLikeDAY7(Vector<City> toAnalyze){
-					
+	public City maxFeelsLikeWeek(Vector<City> toAnalyze) throws InvalidStringException{
+			
+		if(value.equals("FeelsLike") || value.equals("Fl") || value.equals("percepita")) {
+
 			City giveback = toAnalyze.elementAt(0);
 			for(City c : toAnalyze)
 				if(this.DAY7(c.getTemps()) > this.DAY7(giveback.getTemps()))
 					giveback = c;
 			return giveback;
-		}
+		}	else throw new InvalidStringException (value+" è una stringa errata! Devi inserire una stringa tra FeelLike/Fl/percepita");
+	}
 /**
 * 
 * Restituisce la città con il massimo dislivello di temperatura percepita del mese
 */
-	public City maxFeelsLikeDAY30(Vector<City> toAnalyze){
-						
+	public City maxFeelsLikeMonth(Vector<City> toAnalyze) throws InvalidStringException{
+			
+		if(value.equals("FeelsLike") || value.equals("Fl") || value.equals("percepita")) {
+
 			City giveback = toAnalyze.elementAt(0);
 			for(City c : toAnalyze)
 				if(this.DAY30(c.getTemps()) > this.DAY30(giveback.getTemps()))
 					giveback = c;
 			return giveback;
-		}
-
+		}	else throw new InvalidStringException (value+" è una stringa errata! Devi inserire una stringa tra FeelLike/Fl/percepita");
+	}
 }
