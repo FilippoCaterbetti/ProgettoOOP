2823cf812ba376a6f8cb1947596f479319573e22
RobertaCantarini <s1095444@studenti.univpm.it>
2021-03-03 19:39:58 +0100

    Create TestCity.java

---
+++ OPENWEATHER/src/test/java/com/project/OPENWEATHER/Model/TestCity.java
@@ -0,0 +1,19 @@
+package com.project.OPENWEATHER.Model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestCity {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
