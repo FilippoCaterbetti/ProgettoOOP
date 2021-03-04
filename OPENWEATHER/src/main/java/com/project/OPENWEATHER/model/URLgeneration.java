3c8a8c22c4c82d73baa280574204d9c6a425bc5e
leandrofalasca <s1098306@studenti.univpm.it>
2021-03-04 19:20:20 +0100

    Update URLgeneration.java

--- OPENWEATHER/src/main/java/com/project/OPENWEATHER/model/URLgeneration.java
+++ OPENWEATHER/src/main/java/com/project/OPENWEATHER/model/URLgeneration.java
@@ -1,8 +1,66 @@
 package com.project.OPENWEATHER.model;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 public class URLgeneration {
 	String url;
 	String ApiKey;
+	
+	/**
+	 * 
+	 * @param name nome della città
+	 * @return String url
+	 */
+	public String url(String name) {
+		
+		URLgeneration urlgen = new URLgeneration();
+		String url = "http://api.openweathermap.org/data/2.5/forecast?q=";
+		
+		String ApiKey = "";
+		urlgen.setApiKey(ApiKey);
+		url += ( name );
+		url += ( "&units=metric" );
+		url += ( "&appid=" + ApiKey);
+		urlgen.setUrl(url);
+		return url;
+	}
+	
+	
+	
+	
+	/**
+	 * 
+	 * Metodo per ottenere le coordinate di una città partendo dal suo nome tramite l'API di OpenWeather
+	 * 
+	 * @param nomeCitta Nome della città della quale vogliamo sapere le coordinate
+	 * @return 
+	 * @return Le coordinate della città cercata in un vettore di double, nel quale, alla posizione 0 c'è 
+	 * la latitudine, alla posizione 1 la longitudine;
+	 * @throws IOException Se si verificano problemi durante l'input/output
+	 * @throws MalformedURLException Se l'URL per la richiesta di dati all'API di OpenWeather non è corretto
+	 * @throws ParseException Se il parsing del body genera eccezioni
+	 */
+	public  void richiesta(String name) throws MalformedURLException, IOException, ParseException{
+		
+		JSONParser parser = new JSONParser();
+		URLConnection OPENWEATHER = new URL(this.url(name)).openConnection();
+		BufferedReader in = new BufferedReader(new InputStreamReader(OPENWEATHER.getInputStream()));
+		
+		String inputLine = in.readLine();
+		
+		JSONObject risposta = (JSONObject) parser.parse(inputLine);
+
+		return;
+	}
 	
 	
 	/**
