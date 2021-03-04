d7ad642d0f6af1fec6a8a9e4a1078041afb6de43
RobertaCantarini <s1095444@studenti.univpm.it>
2021-03-04 19:01:09 +0100

    Update TempMaxAvg.java

--- OPENWEATHER/src/main/java/com/project/OPENWEATHER/StatsAndFilters/TempMaxAvg.java
+++ OPENWEATHER/src/main/java/com/project/OPENWEATHER/StatsAndFilters/TempMaxAvg.java
@@ -21,7 +21,7 @@ import com.project.OPENWEATHER.model.Temperature;
 	Filters v = new Filters();
 	String value = v.getValue();
 		
-	public double DAY1(Vector <Temperature> dati) {
+	public double Day1(Vector <Temperature> dati) {
 		
 		double sommaTemp=0;
 		for(Temperature t : dati)
@@ -32,7 +32,7 @@ import com.project.OPENWEATHER.model.Temperature;
 * Restituisce la media della temperatura massima della settimana
 * 
 */
-	public double DAY7(Vector <Temperature> dati) {
+	public double Day7(Vector <Temperature> dati) {
 		
 		double sommaTemp=0;
 		for(Temperature t : dati)
@@ -45,7 +45,7 @@ import com.project.OPENWEATHER.model.Temperature;
 * 
 */
 	
-	public double DAY30(Vector <Temperature> dati) {
+	public double Day30(Vector <Temperature> dati) {
 		
 		double sommaTemp=0;
 		for(Temperature t : dati)
@@ -63,7 +63,7 @@ import com.project.OPENWEATHER.model.Temperature;
 		if(value.equals("max") || value.equals("MAX") || value.equals("Max")) {
 			City giveback = toAnalyze.elementAt(0);
 			for(City c : toAnalyze)
-				if(this.DAY1(c.getTemps()) > this.DAY1(giveback.getTemps()))
+				if(this.Day1(c.getTemps()) > this.Day1(giveback.getTemps()))
 					giveback = c;
 			return giveback;
 		}	else throw new InvalidStringException (value+" è una stringa errata! Devi inserire una stringa tra max/MAX/Max");
@@ -79,7 +79,7 @@ import com.project.OPENWEATHER.model.Temperature;
 
 			City giveback = toAnalyze.elementAt(0);
 			for(City c : toAnalyze)
-				if(this.DAY7(c.getTemps()) > this.DAY7(giveback.getTemps()))
+				if(this.Day7(c.getTemps()) > this.Day7(giveback.getTemps()))
 					giveback = c;
 			return giveback;
 		}	else throw new InvalidStringException (value+" è una stringa errata! Devi inserire una stringa tra max/MAX/Max");
@@ -94,7 +94,7 @@ import com.project.OPENWEATHER.model.Temperature;
 
 			City giveback = toAnalyze.elementAt(0);
 			for(City c : toAnalyze)
-				if(this.DAY30(c.getTemps()) > this.DAY30(giveback.getTemps()))
+				if(this.Day30(c.getTemps()) > this.Day30(giveback.getTemps()))
 					giveback = c;
 			return giveback;
 		}	else throw new InvalidStringException (value+" è una stringa errata! Devi inserire una stringa tra max/MAX/Max");
