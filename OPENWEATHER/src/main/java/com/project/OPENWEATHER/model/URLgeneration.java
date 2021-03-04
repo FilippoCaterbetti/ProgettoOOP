4873aafe67c00e398c52917e19fd4c3dd54f451c
leandrofalasca <s1098306@studenti.univpm.it>
2021-03-04 19:46:40 +0100

    Update URLgeneration.java

--- OPENWEATHER/src/main/java/com/project/OPENWEATHER/model/URLgeneration.java
+++ OPENWEATHER/src/main/java/com/project/OPENWEATHER/model/URLgeneration.java
@@ -3,6 +3,7 @@ package com.project.OPENWEATHER.model;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -49,13 +50,9 @@ public class URLgeneration {
 	 */
 	public  void richiesta(String name) throws MalformedURLException, IOException, ParseException{
 		
-		JSONParser parser = new JSONParser();
-		URLConnection OPENWEATHER = new URL(this.url(name)).openConnection();
+		URLConnection OPENWEATHER = (HttpURLConnection) new URL(this.url(name)).openConnection();
 		BufferedReader in = new BufferedReader(new InputStreamReader(OPENWEATHER.getInputStream()));
-		
-		String inputLine = in.readLine();
-		
-		JSONObject risposta = (JSONObject) parser.parse(inputLine);
+				
 
 		return;
 	}
