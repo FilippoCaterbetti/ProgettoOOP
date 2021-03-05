09ed8f357640ab73525ededb63356d4e2c61131e
FilippoCaterbetti <s1095055@studenti.univpm.it>
2021-03-05 11:17:54 +0100

    Update FeelsLikeAvg.java

--- OPENWEATHER/src/main/java/com/project/OPENWEATHER/StatsAndFilters/FeelsLikeAvg.java
+++ OPENWEATHER/src/main/java/com/project/OPENWEATHER/StatsAndFilters/FeelsLikeAvg.java
@@ -1,85 +1,182 @@
 package com.project.OPENWEATHER.StatsAndFilters;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Vector;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 import com.project.OPENWEATHER.exception.InvalidStringException;
+import com.project.OPENWEATHER.exception.NotAllowedValueException;
 import com.project.OPENWEATHER.model.City;
 import com.project.OPENWEATHER.model.Temperature;
 
 public class FeelsLikeAvg implements FiltersStatistics{
+			
+	Statistics statistic = new Statistics();
+/**
+ * 
+ * Questo metodo calcola la media delle 
+ * temperature percepita delle città in un giorno 
+ * 
+ */
+	public JSONArray Day1Avg (ArrayList<String> cities, String value) throws NotAllowedValueException {
+
+JSONArray array = new JSONArray();
+		
+		ArrayList<JSONObject> avg = new ArrayList<JSONObject>();
+		ArrayList<Double> avgFeelsLike = new ArrayList<Double>();
+		ArrayList<JSONObject> objects = new ArrayList<JSONObject>();
+		ArrayList<String> names = new ArrayList<String>();
+		
+		Iterator<String> it = cities.iterator();
+		
+		double parameter1 = 0;
+		double parameter2 = 5000;
 	
-	Filters v = new Filters();
-	String value = v.getValue();
+		int i = 0;
+		
+		while(it.hasNext()) {
+			JSONObject object = new JSONObject();
+			object = statistic.todayAvg(it.next());
+			avg.add(object);
+			double ave = object.getDouble("Feels_like Average");
+			avgFeelsLike.add(ave);
+			
+			JSONObject obj = new JSONObject();
+			obj.put("cityName:", cities.get(i)); 
+			obj.put("feels_like_average:",ave);
+			objects.add(obj);
+			array.put(obj);
+			
+			if(value.equals("max") || value.equals("MAX") || value.equals("Max")) {
+				
+					if(ave>parameter1) {
+						parameter1 = ave;
+						names = new ArrayList<String>();
+						names.add(cities.get(i));
+					}
+					else if(ave==parameter1) {
+						names.add(cities.get(i));
+					}
+					i++;
+				
+			}
+			else if(value.equals("min") || value.equals("MIN") || value.equals("Min")) {
+				
+				if(ave<parameter2) {
+					parameter2 = ave;
+					names = new ArrayList<String>();
+					names.add(cities.get(i));
+				}
+				else if(ave==parameter2) {
+					names.add(cities.get(i));
+				}
+				i++;
+			}
+			else throw new NotAllowedValueException (value+" è una stringa errata! Devi inserire una stringa tra max/MAX/Max oppure min/MIN/Min");
+				
+		}
+		
+		JSONObject object = new JSONObject();
+		
+		if(value.equals("max") || value.equals("MAX") || value.equals("Max")) {
+			object.put("Città con la media più alta", names);
+			object.put("media massima", parameter1);
+		}
+		else { 
+			object.put("Città con media minima ", names);
+			object.put("media minima", parameter2);
+		}
+		
+		
+		array.put(object);
+		
+		
+		return array;
+
+	}
+/**
+* 
+* Questo metodo calcola la media delle 
+* temperature percepita delle città in 5 giorno 
+* 
+*/
 	
-	public double Day1(Vector <Temperature> dati) {
+	public JSONArray Day5Avg (ArrayList<String> cities, String value) throws NotAllowedValueException {
+		JSONArray array = new JSONArray();
 		
-		double sommaTemp=0;
-		for(Temperature t : dati)
-			sommaTemp += t.getTemp();
-		return (sommaTemp/dati.size());
-	}
-
-	public double Day7(Vector <Temperature> dati) {
+		ArrayList<JSONObject> average = new ArrayList<JSONObject>();
+		ArrayList<Double> averageFeelsLike = new ArrayList<Double>();
+		ArrayList<JSONObject> objects = new ArrayList<JSONObject>();
+		ArrayList<String> names = new ArrayList<String>();
 		
-		double sommaTemp=0;
-		for(Temperature t : dati)
-			sommaTemp += t.getTemp();
-		return (sommaTemp/dati.size());
-	}
-
-	public double Day30(Vector <Temperature> dati) {
+		Iterator<String> it = cities.iterator();
 		
-		double sommaTemp=0;
-		for(Temperature t : dati)
-			sommaTemp += t.getTemp();
-		return (sommaTemp/dati.size());
-	}
-/**
-* 
-* Restituisce la città con il massimo dislivello di temperatura percepita del primo giorno 
-*/
-	public City maxFeelsLikeOneday(Vector<City> toAnalyze) throws InvalidStringException{
+		double number1 = 0;
+		double number2 = 3000;
+		
+		int i = 0;
+		
+		while(it.hasNext()) {
+			JSONObject object = new JSONObject();
+			object = statistic.fiveDayAvg(it.next());
+			average.add(object);
+			double ave = object.getDouble("Feels_like Average");
+			averageFeelsLike.add(ave);
 			
-		if(value.equals("FeelsLike") || value.equals("Fl") || value.equals("percepita")) {
-
-			City giveback = toAnalyze.elementAt(0);
-			for(City c : toAnalyze)
-				if(this.Day1(c.getTemps()) > this.Day1(giveback.getTemps()))
-					giveback = c;
-			return giveback;
-		}	else throw new InvalidStringException (value+" è una stringa errata! Devi inserire una stringa tra FeelLike/Fl/percepita");
-	}
-/**
-* 
-* Restituisce la città con il massimo dislivello di temperatura percepita della settimana
-*/
-	public City maxFeelsLikeWeek(Vector<City> toAnalyze) throws InvalidStringException{
+			JSONObject obj = new JSONObject();
+			obj.put("name:",cities.get(i));
+			obj.put("feels_like_avg:",ave);
+			objects.add(obj);
+			array.put(obj);
 			
-		if(value.equals("FeelsLike") || value.equals("Fl") || value.equals("percepita")) {
-
-			City giveback = toAnalyze.elementAt(0);
-			for(City c : toAnalyze)
-				if(this.Day7(c.getTemps()) > this.Day7(giveback.getTemps()))
-					giveback = c;
-			return giveback;
-		}	else throw new InvalidStringException (value+" è una stringa errata! Devi inserire una stringa tra FeelLike/Fl/percepita");
-	}
-/**
-* 
-* Restituisce la città con il massimo dislivello di temperatura percepita del mese
-*/
-	public City maxFeelsLikeMonth(Vector<City> toAnalyze) throws InvalidStringException{
-			
-		if(value.equals("FeelsLike") || value.equals("Fl") || value.equals("percepita")) {
-
-			City giveback = toAnalyze.elementAt(0);
-			for(City c : toAnalyze)
-				if(this.Day30(c.getTemps()) > this.Day30(giveback.getTemps()))
-					giveback = c;
-			return giveback;
-		}	else throw new InvalidStringException (value+" è una stringa errata! Devi inserire una stringa tra FeelLike/Fl/percepita");
-	}
-}
+			if(value.equals("max") || value.equals("MAX") || value.equals("Max")) {
+				
+				if(ave>number1) {
+					number1 = ave;
+					names = new ArrayList<String>();
+					names.add(cities.get(i));
+				}
+				else if(ave==number1) {
+					names.add(cities.get(i));
+				}
+				i++;
+				
+			}
+			else if(value.equals("min") || value.equals("MIN") || value.equals("Min")) {
+				
+				if(ave<number2) {
+					number2 = ave;
+					names = new ArrayList<String>();
+					names.add(cities.get(i));
+				}
+				else if(ave==number2) {
+					names.add(cities.get(i));
+				}
+				i++;
+			}
+			else throw new NotAllowedValueException (value+" è una stringa errata! Devi inserire una stringa tra max/MAX/Max oppure min/MIN/Min");
+				
+		}
+		
+		JSONObject object = new JSONObject();
+		
+		if(value.equals("max") || value.equals("MAX") || value.equals("Max")) {
+			object.put("Città con la media più alta", names);
+			object.put("media massima", number1);
+		}
+		else { 
+			object.put("Città con la media minima", names);
+			object.put("media minima", number2);
+		}
+		
+		
+		array.put(object);
+		
+		return array;
+		
+	}		
+}	
+	
