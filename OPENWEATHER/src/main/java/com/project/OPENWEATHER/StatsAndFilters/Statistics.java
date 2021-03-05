815df0cf8cfa05dfa40d46517e322a5205ebbc45
FilippoCaterbetti <s1095055@studenti.univpm.it>
2021-03-05 11:18:03 +0100

    Create Statistics.java

---
+++ OPENWEATHER/src/main/java/com/project/OPENWEATHER/StatsAndFilters/Statistics.java
@@ -0,0 +1,30 @@
+package com.project.OPENWEATHER.StatsAndFilters;
+
+import java.util.Vector;
+
+import org.json.JSONObject;
+
+import com.project.OPENWEATHER.error.ErrorCalculator;
+import com.project.OPENWEATHER.exception.InvalidStringException;
+import com.project.OPENWEATHER.model.City;
+import com.project.OPENWEATHER.model.Temperature;
+import com.project.OPENWEATHER.service.ServiceApplication;
+
+public class Statistics extends ErrorCalculator{
+	ServiceApplication service = new ServiceApplication();
+	
+	
+	public JSONObject todayAvg(String next) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public JSONObject fiveDayAvg(String next) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+		
+	
+
+}
+
