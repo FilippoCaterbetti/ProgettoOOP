8dae5daceb55a8164df252068839592d2a54df31
RobertaCantarini <s1095444@studenti.univpm.it>
2021-03-06 01:03:46 +0100

    Update CitynotFoundException.java

--- OPENWEATHER/src/main/java/com/project/OPENWEATHER/exception/CitynotFoundException.java
+++ OPENWEATHER/src/main/java/com/project/OPENWEATHER/exception/CitynotFoundException.java
@@ -3,19 +3,20 @@ package com.project.OPENWEATHER.exception;
 import java.lang.Exception;
 
 
-//generiamo questo errore se la città è errata o non esiste
+  /**generiamo l'errore se la città è errata o non esiste
+   *
+   */
 
 public class CitynotFoundException extends Exception{
 	
 	String error;
 	
 	/**
-	 * @param error è il messaggio di errore.
+	 * @param error rappresenta il messaggio di errore.
 	 */
 	public CitynotFoundException(String error) {
 		this.error=error;
 	}
-	
 	
 	/**
 	 * @return String con messaggio di errore
