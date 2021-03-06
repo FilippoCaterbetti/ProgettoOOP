1e0500d7a3bc00467c32da670a6a1ef4a2e4485a
FilippoCaterbetti <s1095055@studenti.univpm.it>
2021-03-06 17:49:48 +0100

    Update TempMinAvg.java

--- OPENWEATHER/src/main/java/com/project/OPENWEATHER/StatsAndFilters/TempMinAvg.java
+++ OPENWEATHER/src/main/java/com/project/OPENWEATHER/StatsAndFilters/TempMinAvg.java
@@ -1,11 +1,14 @@
 package com.project.OPENWEATHER.StatsAndFilters;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Vector;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.json.simple.parser.ParseException;
 
 import com.project.OPENWEATHER.exception.InvalidStringException;
 import com.project.OPENWEATHER.exception.NotAllowedValueException;
@@ -21,9 +24,12 @@ public class TempMinAvg implements FiltersStatistics{
 	* @param value
 	* @return
 	* @throws NotAllowedValueException
+	 * @throws ParseException 
+	 * @throws IOException 
+	 * @throws MalformedURLException 
 	*/
 
-		public JSONArray Day1Avg (ArrayList<String> cities, String value) throws NotAllowedValueException {
+		public JSONArray Day1Avg (ArrayList<String> cities, String value) throws NotAllowedValueException, MalformedURLException, IOException, ParseException {
 
 			JSONArray array = new JSONArray();
 						
@@ -103,9 +109,12 @@ public class TempMinAvg implements FiltersStatistics{
 		* 
 		* Questo metodo calcola la media delle 
 		* temperature percepita delle città in 5 giorno 
+		 * @throws ParseException 
+		 * @throws IOException 
+		 * @throws MalformedURLException 
 		* 
 		*/		
-			public JSONArray Day5Avg (ArrayList<String> cities, String value) throws NotAllowedValueException {
+			public JSONArray Day5Avg (ArrayList<String> cities, String value) throws NotAllowedValueException, MalformedURLException, IOException, ParseException {
 						JSONArray array = new JSONArray();
 						
 						ArrayList<JSONObject> average = new ArrayList<JSONObject>();
