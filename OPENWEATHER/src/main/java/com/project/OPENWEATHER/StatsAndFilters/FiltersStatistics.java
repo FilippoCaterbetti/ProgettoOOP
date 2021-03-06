de3e30b4c3377c694cd6996dd8fb3a81a93fa93b
FilippoCaterbetti <s1095055@studenti.univpm.it>
2021-03-06 17:49:36 +0100

    Update FiltersStatistics.java

--- OPENWEATHER/src/main/java/com/project/OPENWEATHER/StatsAndFilters/FiltersStatistics.java
+++ OPENWEATHER/src/main/java/com/project/OPENWEATHER/StatsAndFilters/FiltersStatistics.java
@@ -1,9 +1,12 @@
 package com.project.OPENWEATHER.StatsAndFilters;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.Vector;
 
 import org.json.JSONArray;
+import org.json.simple.parser.ParseException;
 
 import com.project.OPENWEATHER.exception.InvalidStringException;
 import com.project.OPENWEATHER.exception.NotAllowedValueException;
@@ -11,7 +14,7 @@ import com.project.OPENWEATHER.model.Temperature;
 
 public interface  FiltersStatistics {
 
-	public abstract JSONArray Day1Avg(ArrayList<String> cities, String value) throws NotAllowedValueException;
-	public abstract JSONArray Day5Avg(ArrayList<String> cities, String value) throws NotAllowedValueException;
+	public abstract JSONArray Day1Avg(ArrayList<String> cities, String value) throws NotAllowedValueException, MalformedURLException, IOException, ParseException;
+	public abstract JSONArray Day5Avg(ArrayList<String> cities, String value) throws NotAllowedValueException, MalformedURLException, IOException, ParseException;
 
 }
