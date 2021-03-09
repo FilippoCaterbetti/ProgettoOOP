673b7fac75ef4b28573f5e4ad9cbc9c354bc1da4
FilippoCaterbetti <s1095055@studenti.univpm.it>
2021-03-09 19:42:59 +0100

    Update OpenweatherApplication.java

--- OPENWEATHER/src/main/java/com/project/OPENWEATHER/OpenweatherApplication.java
+++ OPENWEATHER/src/main/java/com/project/OPENWEATHER/OpenweatherApplication.java
@@ -3,6 +3,9 @@ package com.project.OPENWEATHER;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.project.OPENWEATHER.service.ServiceApplication;
+
+
 @SpringBootApplication
 public class OpenweatherApplication {
 
