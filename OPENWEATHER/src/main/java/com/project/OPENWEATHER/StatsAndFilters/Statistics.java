c97c28caebd4289e5c127c7d9b4e622bdfd4311c
FilippoCaterbetti <s1095055@studenti.univpm.it>
2021-03-09 18:06:57 +0100

    Update Statistics.java

--- OPENWEATHER/src/main/java/com/project/OPENWEATHER/StatsAndFilters/Statistics.java
+++ OPENWEATHER/src/main/java/com/project/OPENWEATHER/StatsAndFilters/Statistics.java
@@ -18,7 +18,6 @@ public class Statistics extends ErrorCalculator{
 	ServiceApplication service = new ServiceApplication();
 	 
 	/**
-	 
 	 * @param next è il nome della città che viene passata della quale si vuole sapere la media delle statistiche raccolte
 	 * @return
 	 * @throws MalformedURLException
