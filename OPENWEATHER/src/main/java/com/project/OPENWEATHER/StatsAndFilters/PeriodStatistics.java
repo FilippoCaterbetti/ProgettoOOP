a067f4fc72de53613166d490657035735b305e45
RobertaCantarini <s1095444@studenti.univpm.it>
2021-03-09 10:47:55 +0100

    Update PeriodStatistics.java

--- OPENWEATHER/src/main/java/com/project/OPENWEATHER/StatsAndFilters/PeriodStatistics.java
+++ OPENWEATHER/src/main/java/com/project/OPENWEATHER/StatsAndFilters/PeriodStatistics.java
@@ -2,27 +2,27 @@ package com.project.OPENWEATHER.StatsAndFilters;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
-//MODIFICA!!!!!!!!
+
 public class PeriodStatistics {
 
 		/**
-		 * Questo metodo restituisce il JSONArray con le statistiche sulla visibilità di tutti i giorni presenti nello storico.
+		 * Questo metodo restituisce il JSONArray con le statistiche sulle temperature di tutti i giorni presenti nello storico.
 		 * @param city rappresenta il nome della città di cui si vuole fare statistica.
-		 * @param visibility contiene tutte le informazioni sulla visibilità presenti nello storico.
+		 * @param temp contiene tutte le informazioni sulla visibilità presenti nello storico.
 		 * @return un JSONArray con le statistiche giorno per giorno.
 		 */
-		public JSONArray DailyStats(String city, JSONArray visibility) {
+		public JSONArray DailyStats(String city, JSONArray temp) {
 			
 			JSONArray stats = new JSONArray();
-			int i=0;
+			
 			String date = "";
 			
 			stats.put(city);
 			
-			while(i<visibility.length()) {
+			for(int i = 0;i < temp.length(); i++) {
 				
 				JSONArray oneDayVisibility = new JSONArray();
-				oneDayVisibility = visibility.getJSONArray(i);
+				oneDayVisibility = temp.getJSONArray(i);
 				
 				int average=0;
 				int j=0;
@@ -31,14 +31,17 @@ public class PeriodStatistics {
 				int variance=0;
 				
 				while(j<oneDayVisibility.length()) {
+					
 					JSONObject hour = new JSONObject();
 					hour = oneDayVisibility.getJSONObject(j);
-					average += hour.getInt("visibility");
+					average += hour.getInt("temperature");
 					date = hour.getString("data").substring(0,10);
-					if(hour.getInt("visibility")>max)
-						max = hour.getInt("visibility");
-					if(hour.getInt("visibility")<min)
-						min = hour.getInt("visibility");
+					
+					if(hour.getInt("temperature") > max)
+						max = hour.getInt("temperature");
+					
+					if(hour.getInt("temperature") < min)
+						min = hour.getInt("temperature");
 					
 					j++;
 				}
@@ -51,12 +54,12 @@ public class PeriodStatistics {
 				info.put("min", min);
 				info.put("average", average);
 				
-				j=0;
-				while(j<oneDayVisibility.length()) {
+				while(j < oneDayVisibility.length()) {
+					
 					JSONObject hour = new JSONObject();
 					hour = oneDayVisibility.getJSONObject(j);
-					hour.getInt("visibility");
-			        variance += ((hour.getInt("visibility")-average))^2;
+					hour.getInt("temperature");
+			        variance += ((hour.getInt("temperature")-average))^2;
 			       	j++;
 			     }
 				
@@ -64,8 +67,7 @@ public class PeriodStatistics {
 				info.put("variance", variance);
 				
 				stats.put(info);
-				
-				i++;
+			
 			}
 			
 			return stats;
@@ -73,16 +75,16 @@ public class PeriodStatistics {
 		
 		
 		/**
-		 * Questo metodo restituisce il JSONArray con le statistiche sulla visibilità di ogni settimana presente nello storico.
+		 * Questo metodo restituisce il JSONArray con le statistiche sulle temperature di ogni settimana presente nello storico.
 		 * Richiama il metodo che va a calcolare la statistica giorno per giorno.
 		 * @param city rappresenta il nome della città di cui si vuole fare statistica.
-		 * @param visibility contiene tutte le informazioni sulla visibilità presenti nello storico.
+		 * @param temp contiene tutte le informazioni sulla visibilità presenti nello storico.
 		 * @return un JSONArray con le statistiche settimana per settimana.
 		 */
-		public JSONArray  OneWeekStats(String city, JSONArray visibility) {
+		public JSONArray  OneWeekStats(String city, JSONArray temp) {
 			
 			
-			JSONArray oneDay = DailyStats(city,visibility);
+			JSONArray oneDay = DailyStats(city,temp);
 			JSONArray stats = new JSONArray();
 			
 			
@@ -91,26 +93,30 @@ public class PeriodStatistics {
 			int week = 1;
 			stats.put(oneDay.get(0));
 			
-			while(i<15) {
+			while(i < 21) {
 
 				int average = 0;
 				int max = 0;
 				int min = 10000;
 				int variance = 0;
 				
-				while(k<(8+i)) {
+				while(k < (8 + i) ) {
 					
 						JSONObject dayInfo = new JSONObject();
 						dayInfo = oneDay.getJSONObject(k);
 						average += dayInfo.getInt("average");
-						if(dayInfo.getInt("max")>max)
+						
+						if(dayInfo.getInt("max") > max)
 							max = dayInfo.getInt("max");
-						if(dayInfo.getInt("min")<min)
+						
+						if(dayInfo.getInt("min") < min)
 							min = dayInfo.getInt("min");
+						
 						variance += dayInfo.getInt("variance");	
 						k++;
 					
 				}
+				
 				average /= 7;
 				variance /= 7;
 				
@@ -135,17 +141,17 @@ public class PeriodStatistics {
 		
 		
 		/**
-		 * Questo metodo restituisce il JSONArray con le statistiche sulla visibilità delle tre settimane presenti nello storico.
+		 * Questo metodo restituisce il JSONArray con le statistiche sulle temperature del mese presenti nello storico.
 		 * Richiama il metodo che va a calcolare la statistica settimana per settimana.
 		 * @param city rappresenta il nome della città di cui si vuole fare statistica.
 		 * @param visibility contiene tutte le informazioni sulla visibilità presenti nello storico.
 		 * @return un JSONArray con le statistiche per tre settimane.
 		 */
-		public JSONArray  OneMonthStats(String city, JSONArray visibility) {
+		public JSONArray  OneMonthStats(String city, JSONArray temp) {
 			
 			JSONArray stats = new JSONArray();
 			JSONArray oneWeek = new JSONArray();
-			oneWeek = OneWeekStats(city, visibility);
+			oneWeek = OneWeekStats(city, temp);
 			
 			stats.put(oneWeek.get(0));
 			
@@ -155,22 +161,24 @@ public class PeriodStatistics {
 			int variance = 0;
 			
 			int i = 1;
-			while(i<oneWeek.length()) {
+			while(i < oneWeek.length()) {
+				
 				JSONObject info = new JSONObject();
 				info = oneWeek.getJSONObject(i);
 				average += info.getInt("average");
 				variance += info.getInt("variance");
 				
-				if(info.getInt("max")>max)
+				if(info.getInt("max") > max)
 					max = info.getInt("max");
-				if(info.getInt("min")<min)
+				
+				if(info.getInt("min") < min)
 					min = info.getInt("min");
 				i++;
 					
 			}
 			
-			average /= 3;
-			variance /= 3;
+			average /= 4;
+			variance /= 4;
 			
 			JSONObject visibilityInfo = new JSONObject();
 			visibilityInfo.put("average", average);
