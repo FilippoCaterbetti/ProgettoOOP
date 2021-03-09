44e50ad5bc0fb816ebd6bc8c4d88c0d0ecc79171
FilippoCaterbetti <s1095055@studenti.univpm.it>
2021-03-09 19:42:51 +0100

    Update URLgeneration.java

--- OPENWEATHER/src/main/java/com/project/OPENWEATHER/model/URLgeneration.java
+++ OPENWEATHER/src/main/java/com/project/OPENWEATHER/model/URLgeneration.java
@@ -14,7 +14,7 @@ import org.json.simple.parser.ParseException;
 
 public class URLgeneration {
 	String url;
-	String ApiKey = "";
+	String ApiKey = "069782b0f7fc9729e6c7151ffd1448ed";
 	
 	/**
 	 * 
