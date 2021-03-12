4642c7107fa6593ac2b066190352413557caa786
FilippoCaterbetti <s1095055@studenti.univpm.it>
2021-03-12 11:41:28 +0100

    Update OpenweatherApplication.java

--- OPENWEATHER/src/main/java/com/project/OPENWEATHER/OpenweatherApplication.java
+++ OPENWEATHER/src/main/java/com/project/OPENWEATHER/OpenweatherApplication.java
@@ -3,11 +3,17 @@ package com.project.OPENWEATHER;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.project.OPENWEATHER.service.ServiceApplication;
+
 @SpringBootApplication(scanBasePackages={"com.project.OPENWEATHER.controller", "com.project.OPENWEATHER.service","com.project.OPENWEATHER.model"})
 public class OpenweatherApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OpenweatherApplication.class, args);
-		
+		ServiceApplication service = new ServiceApplication();
+		service.FiveHoursInfo("Ancona");
+		service.FiveHoursInfo("Milano");
+		service.FiveHoursInfo("Roma");
+		service.FiveHoursInfo("Bologna");
 	}
 }
