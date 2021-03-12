c65ea65a070b13cc3bac42d4bd26140e9d42f454
leandrofalasca <s1098306@studenti.univpm.it>
2021-03-12 16:09:53 +0100

    Update TempMinAvg.java

--- OPENWEATHER/src/main/java/com/project/OPENWEATHER/StatsAndFilters/TempMinAvg.java
+++ OPENWEATHER/src/main/java/com/project/OPENWEATHER/StatsAndFilters/TempMinAvg.java
@@ -21,7 +21,6 @@ public class TempMinAvg implements FiltersStatistics{
 	/**
 	* 
 	* @param cities
-	* @param value
 	* @return
 	* @throws NotAllowedValueException
 	 * @throws ParseException 
@@ -40,9 +39,7 @@ public class TempMinAvg implements FiltersStatistics{
 						
 						Iterator<String> it = cities.iterator();
 						
-						//double parameter1 = 0;
-						//double parameter2 = 5000;
-					
+						
 						int i = 0;
 						
 						while(it.hasNext()) {
@@ -63,54 +60,7 @@ public class TempMinAvg implements FiltersStatistics{
 						return array;
 
 					}
-						/**
-						 * if(value.equals("max") || value.equals("MAX") || value.equals("Max")) {
-								
-									if(ave>parameter1) {
-										parameter1 = ave;
-										names = new ArrayList<String>();
-										names.add(cities.get(i));
-									}
-									else if(ave==parameter1) {
-										names.add(cities.get(i));
-									}
-									i++;
-								
-							}
-							else if(value.equals("min") || value.equals("MIN") || value.equals("Min")) {
-								
-								if(ave<parameter2) {
-									parameter2 = ave;
-									names = new ArrayList<String>();
-									names.add(cities.get(i));
-								}
-								else if(ave==parameter2) {
-									names.add(cities.get(i));
-								}
-								i++;
-							}
-							else throw new NotAllowedValueException (value+" è una stringa errata! Devi inserire una stringa tra max/MAX/Max oppure min/MIN/Min");
-								
-						}
 						
-						JSONObject object = new JSONObject();
-						
-						if(value.equals("max") || value.equals("MAX") || value.equals("Max")) {
-							
-							object.put("Città con la media più alta", names);
-							object.put("media massima", parameter1);
-						}
-						else { 
-							
-							object.put("Città con media minima ", names);
-							object.put("media minima", parameter2);
-						}
-						
-						
-						array.put(object);
-						
-						 * 
-						 */
 		/**
 		* 
 		* Questo metodo calcola la media delle 
@@ -130,8 +80,6 @@ public class TempMinAvg implements FiltersStatistics{
 						
 						Iterator<String> it = cities.iterator();
 						
-						//double number1 = 0;
-						//double number2 = 3000;
 						
 						int i = 0;
 						
@@ -154,55 +102,3 @@ public class TempMinAvg implements FiltersStatistics{
 						return array;
 			}
 }
-
-/**
- * if(value.equals("max") || value.equals("MAX") || value.equals("Max")) {
-								
-								if(ave>number1) {
-									number1 = ave;
-									names = new ArrayList<String>();
-									names.add(cities.get(i));
-								}
-								else if(ave==number1) {
-									
-									names.add(cities.get(i));
-								}
-								i++;
-								
-							}
-							else if(value.equals("min") || value.equals("MIN") || value.equals("Min")) {
-								
-								if(ave<number2) {
-									number2 = ave;
-									names = new ArrayList<String>();
-									names.add(cities.get(i));
-								}
-								else if(ave==number2) {
-									
-									names.add(cities.get(i));
-								}
-								i++;
-							}
-							else throw new NotAllowedValueException (value+" è una stringa errata! Devi inserire una stringa tra max/MAX/Max oppure min/MIN/Min");
-								
-						}
-						
-						JSONObject object = new JSONObject();
-						
-						if(value.equals("max") || value.equals("MAX") || value.equals("Max")) {
-							
-							object.put("Città con la media più alta", names);
-							object.put("media massima", number1);
-						}
-						else { 
-							
-							object.put("Città con la media minima", names);
-							object.put("media minima", number2);
-						}
-						
-						
-						array.put(object);
- * 
- */
-		
-
