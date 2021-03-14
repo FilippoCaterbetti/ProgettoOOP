d93d773962eb62356a51761db01f5851d1d3b93a
leandrofalasca <s1098306@studenti.univpm.it>
2021-03-14 12:24:37 +0100

    Update Statistics.java

--- OPENWEATHER/src/main/java/com/project/OPENWEATHER/StatsAndFilters/Statistics.java
+++ OPENWEATHER/src/main/java/com/project/OPENWEATHER/StatsAndFilters/Statistics.java
@@ -135,69 +135,97 @@ public class Statistics extends ErrorCalculator{
 		City city = new City(next);
 		 
 	     city = service.getTempFutureApi(next);
+	     
 	     double temp_max_avg = 0;
 	     double temp_min_avg = 0;
 	     double feels_like_avg = 0;
 	     double real_temp_avg = 0;
-	     double variance = 0;
+
+	     double variancefeelslike = 0;
+	     double variancetemp= 0;
+	     
+	     
 	     
 	     int i=0;
 	     
-	     String date = "";
-	     date += (city.getTemps().get(0).getData()).charAt(8);
-	     date += (city.getTemps().get(0).getData()).charAt(9);
+	     double max_temp = 0;
+	     double min_temp = city.getTemps().get(i).getTemp();
+	     double max_feelslike = 0;
+	     double min_feelslike = city.getTemps().get(i).getFeels_like();
+	     
+	     
 	 
-	     String datas = date;
-	     while(date.equals(datas)) {
-	        
+
+	     while( i<city.getTemps().size()) {
+	         
 	    	 temp_max_avg += city.getTemps().get(i).getTemp_max();
 	         temp_min_avg += city.getTemps().get(i).getTemp_min();
 	         feels_like_avg += city.getTemps().get(i).getFeels_like();
 	         real_temp_avg += city.getTemps().get(i).getTemp(); 
+	         
+	         if(city.getTemps().get(i).getTemp()>max_temp) {
+	        	 
+	        	 max_temp = city.getTemps().get(i).getTemp();
+
+	         }
+	         if (city.getTemps().get(i).getTemp()<min_temp) {
+	        	 
+	        	 min_temp = city.getTemps().get(i).getTemp();
+
+	         }
+	         if(city.getTemps().get(i).getFeels_like()>max_feelslike) {
+	        	 
+	        	 max_feelslike = city.getTemps().get(i).getFeels_like();
+
+	         }
+	         if (city.getTemps().get(i).getFeels_like()<min_feelslike) {
+	        	 
+	        	 min_feelslike = city.getTemps().get(i).getFeels_like();
+
+	         }
 	         i++;
-	         datas = "";
-	         datas += (city.getTemps().get(i).getData()).charAt(8);
-	         datas += (city.getTemps().get(i).getData()).charAt(9);
 	     }
 	     
 	     temp_max_avg = temp_max_avg/i;
 	     temp_min_avg = temp_min_avg/i;
 	     feels_like_avg = feels_like_avg/i;
 	     real_temp_avg = real_temp_avg/i;
-	     datas = date;
+	     
 	     i=0;
 	     
-	     //calcolo della varianza di visibilità
-	     while(date.equals(datas)) {
-	     	i++;
-	     	datas = "";
-	     	datas += (city.getTemps().get(i).getData()).charAt(8);
-	     	datas += (city.getTemps().get(i).getData()).charAt(9);
-	     	
-	     }
-	   //calcolo della varianza della temperatura percepita
 	     while(i<city.getTemps().size()) {
-	     	variance += ((int)((city.getTemps().get(i).getTemp())-real_temp_avg))^2;
-	     	i++;
-	     }
+	    	 
+	    	 variancetemp = ((int)((city.getTemps().get(i).getTemp())-real_temp_avg))^2;
+	    	 variancefeelslike = ((int)((city.getTemps().get(i).getFeels_like())-feels_like_avg))^2;
+
+	    	 i++;
+	       }
+	  
+	     variancetemp /=i;
+	     variancefeelslike /=i;
 	     
-	     variance /=i;
-	     
-	   //calcolo della varianza della temperatura reale
-	     while(i<city.getTemps().size()) {
-	     	variance += ((int)((city.getTemps().get(i).getFeels_like())-feels_like_avg))^2;
-	     	i++;
-	     }
-	     
-	     variance /=i;     
+  
 	     JSONObject object = new JSONObject();
+	     JSONObject temp_data = new JSONObject();
 	     
-	     object.put("CityName", next);
+	     
+	     temp_data.put("Real temp average",real_temp_avg);
+	     temp_data.put("Max Real temp",max_temp);
+	     temp_data.put("Min Real temp", min_temp);
+	     temp_data.put("Real temp variance", variancetemp);
+	     
+	     temp_data.put("Feels like average",feels_like_avg);
+	     temp_data.put("Max feels like",max_feelslike);
+	     temp_data.put("Min feels like", min_feelslike);
+	     temp_data.put("Feels like variance", variancefeelslike);
+	     
+	     
+	     object.put("City", next);
 	     object.put("Temp_Max Average", temp_max_avg);
 	     object.put("Temp_Min Average", temp_min_avg);
-	     object.put("Feels_like Average", feels_like_avg);  
-	     object.put("Real_temp Average", real_temp_avg);  
+	     object.put("Temp and Feels like data", temp_data);
 	     return object;
+	 
 	 	}
 	
 	}	
