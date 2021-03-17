3b4a1abc7fa057d3fc1d3ce8294a4fe73192fa9b
RobertaCantarini <s1095444@studenti.univpm.it>
2021-03-17 18:17:25 +0100

    Update JSONClass.java

--- OPENWEATHER/src/main/java/com/project/OPENWEATHER/service/JSONClass.java
+++ OPENWEATHER/src/main/java/com/project/OPENWEATHER/service/JSONClass.java
@@ -1,6 +1,5 @@
 package com.project.OPENWEATHER.service;
 
-
 import java.util.Vector;
 
 import org.json.JSONObject;
@@ -10,50 +9,45 @@ import com.project.OPENWEATHER.model.City;
 import org.json.JSONArray;
 
 public class JSONClass {
-	
+
 	City city = new City();
-	
-	
+
 	/**
 	 * 
-	 * JSONObject  oggetto City
+	 * JSONObject oggetto City
 	 * 
 	 * @param name della città in JSONObject.
-	 * @return il JSONObject  della città.
+	 * @return il JSONObject della città.
 	 * 
 	 */
 	public JSONObject JSONClass(City city) {
-		
-		
+
 		JSONObject object = new JSONObject();
-		
+
 		object.put("name", city.getName());
 		object.put("country", city.getCountry());
 		object.put("id", city.getId());
-		
+
 		JSONArray arr = new JSONArray();
-		
-		for(int i=0; i<(city.getTemps()).size(); i++) {
-			
+
+		for (int i = 0; i < (city.getTemps()).size(); i++) {
+
 			JSONObject ow = new JSONObject();
-			
+
 			ow.put("temp", (city.getTemps().get(i).getTemp()));
 			ow.put("feels_like", (city.getTemps()).get(i).getFeels_like());
 			ow.put("temp_max", (city.getTemps()).get(i).getTemp_max());
 			ow.put("temp_min", (city.getTemps()).get(i).getTemp_min());
 			ow.put("temp_avg", (city.getTemps()).get(i).getTemp_avg());
 			ow.put("data", (city.getTemps()).get(i).getData());
-			
 
 			arr.put(ow);
-			
+
 		}
-		
-		
+
 		object.put("OpenWeather", arr);
-		
+
 		return object;
 	}
-	
 
 }
