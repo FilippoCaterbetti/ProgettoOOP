6a3715c14aec2605d53785a2c808ccf4321b51ab
RobertaCantarini <s1095444@studenti.univpm.it>
2021-03-17 18:17:23 +0100

    Update Service.java

--- OPENWEATHER/src/main/java/com/project/OPENWEATHER/service/Service.java
+++ OPENWEATHER/src/main/java/com/project/OPENWEATHER/service/Service.java
@@ -1,13 +1,10 @@
 package com.project.OPENWEATHER.service;
 
-
 import java.io.FileNotFoundException;
-
 
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
-
 
 import java.util.concurrent.locks.AbstractQueuedSynchronizer.ConditionObject;
 
@@ -23,17 +20,30 @@ import com.project.OPENWEATHER.exception.NotAllowedValueException;
 import com.project.OPENWEATHER.model.City;
 
 public interface Service {
-	
+
 	public abstract JSONObject getCityApi(String name);
+
 	public abstract JSONArray getTempApi(String name);
+
 	public String FiveHoursInfo(String name);
+
 	public City getCityInfofromApi(String name);
-	public ArrayList<JSONObject> HistoryOfError(ArrayList<String> names ,int error, String value,int period) throws EmptyStringException, CitynotFoundException, NotAllowedPeriodException, NotAllowedValueException, IOException, InvalidStringException;
-	public abstract City getTempFutureApi(String name) ;
+
+	public ArrayList<JSONObject> HistoryOfError(ArrayList<String> names, int error, String value, int period)
+			throws EmptyStringException, CitynotFoundException, NotAllowedPeriodException, NotAllowedValueException,
+			IOException, InvalidStringException;
+
+	public abstract City getTempFutureApi(String name);
+
 	public JSONArray Substring(String regex) throws FileNotFoundException, IOException, ParseException;
-	public ArrayList<JSONArray> PeriodHistory(ArrayList<String> cities, String period) throws EmptyStringException, CitynotFoundException, NotAllowedPeriodException, IOException, InvalidStringException;
-	public String save(String name) throws IOException, ParseException; 
+
+	public ArrayList<JSONArray> PeriodHistory(ArrayList<String> cities, String period) throws EmptyStringException,
+			CitynotFoundException, NotAllowedPeriodException, IOException, InvalidStringException;
+
+	public String save(String name) throws IOException, ParseException;
+
 	public JSONObject readHistory(String name) throws IOException, InvalidStringException;
+
 	public JSONArray listOfCities();
 
 }
