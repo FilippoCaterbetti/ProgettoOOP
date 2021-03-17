b47ba098a11e4e24779aaaf63999e4608fe5042c
RobertaCantarini <s1095444@studenti.univpm.it>
2021-03-17 18:17:49 +0100

    Update EmptyStringException.java

--- OPENWEATHER/src/main/java/com/project/OPENWEATHER/exception/EmptyStringException.java
+++ OPENWEATHER/src/main/java/com/project/OPENWEATHER/exception/EmptyStringException.java
@@ -1,27 +1,28 @@
-package com.project.OPENWEATHER.exception; 
+package com.project.OPENWEATHER.exception;
 
-   /**
-    * generiamo l'eccezione a causa di una stringa vuota 
-    *
-    */
+/**
+ * generiamo l'eccezione a causa di una stringa vuota
+ *
+ */
 public class EmptyStringException extends Exception {
-	
+
 	String error;
-	
+
 	/**
 	 * @param error rappresenta il messaggio di errore.
 	 */
 	public EmptyStringException(String error) {
-		
+
 		this.error = error;
 	}
-	
+
 	/**
-	 * Restituisce un messaggio di errore 
+	 * Restituisce un messaggio di errore
+	 * 
 	 * @return String che contiene il messaggio d'errore
 	 */
 	public String getError() {
-		
+
 		return error;
 	}
 
