379b58b5e77a85e8661201dcdb7dd35063367e26
RobertaCantarini <s1095444@studenti.univpm.it>
2021-03-17 18:17:08 +0100

    Update RealTempAvg.java

--- OPENWEATHER/src/main/java/com/project/OPENWEATHER/StatsAndFilters/RealTempAvg.java
+++ OPENWEATHER/src/main/java/com/project/OPENWEATHER/StatsAndFilters/RealTempAvg.java
@@ -16,97 +16,96 @@ import com.project.OPENWEATHER.exception.NotAllowedValueException;
 import com.project.OPENWEATHER.model.City;
 import com.project.OPENWEATHER.model.Temperature;
 
-public class RealTempAvg implements FiltersStatistics{
-	
+public class RealTempAvg implements FiltersStatistics {
+
 	Statistics statistic = new Statistics();
 
 	/**
+	 * Questo metodo calcola la media delle temperature reali delle città di 1
+	 * giorno
 	 * 
-	 * @param cities
-	 * @return
-	 * @throws NotAllowedValueException
-	 * @throws ParseException 
-	 * @throws IOException 
-	 * @throws MalformedURLException 
+	 * @param cities contiene le varie città
+	 * @return array con temperatura desiderata
+	 * @throws ParseException        per errore di parsing
+	 * @throws IOException           per errore di input ed output
+	 * @throws MalformedURLException per errore di formazione di URL
 	 */
 
-	public JSONArray Day1Avg (ArrayList<String> cities) throws NotAllowedValueException, MalformedURLException, IOException, ParseException {
+	public JSONArray Day1Avg(ArrayList<String> cities) throws MalformedURLException, IOException, ParseException {
 
 		JSONArray array = new JSONArray();
-				
-				ArrayList<JSONObject> avg = new ArrayList<JSONObject>();
-				ArrayList<Double> avgRealTemp= new ArrayList<Double>();
-				ArrayList<JSONObject> objects = new ArrayList<JSONObject>();
-				ArrayList<String> names = new ArrayList<String>();
-				
-				Iterator<String> it = cities.iterator();
-				
-				
-				int i = 0;
-				
-				while(it.hasNext()) {
-					JSONObject object = new JSONObject();
-					object = statistic.todayAvg(it.next());
-					avg.add(object);
-					double ave = object.getDouble("Real_temp Average");
-					avgRealTemp.add(ave);
-					
-					JSONObject obj = new JSONObject();
-					obj.put("Name:", cities.get(i)); 
-					obj.put("real_temp_avg:",ave);
-					objects.add(obj);
-					array.put(obj);
-					
-					i++;
-					
-				}
-				
-				return array;
 
-			}
-	
-	
+		ArrayList<JSONObject> avg = new ArrayList<JSONObject>();
+		ArrayList<Double> avgRealTemp = new ArrayList<Double>();
+		ArrayList<JSONObject> objects = new ArrayList<JSONObject>();
+		ArrayList<String> names = new ArrayList<String>();
+
+		Iterator<String> it = cities.iterator();
+
+		int i = 0;
+
+		while (it.hasNext()) {
+			JSONObject object = new JSONObject();
+			object = statistic.todayAvg(it.next());
+			avg.add(object);
+			double ave = object.getDouble("Real_temp Average");
+			avgRealTemp.add(ave);
+
+			JSONObject obj = new JSONObject();
+			obj.put("Name:", cities.get(i));
+			obj.put("real_temp_avg:", ave);
+			objects.add(obj);
+			array.put(obj);
+
+			i++;
+
+		}
+
+		return array;
+
+	}
+
 	/**
-	* 
-	* Questo metodo calcola la media delle 
-	* temperature percepita delle città in 5 giorno 
-	 * @throws ParseException 
-	 * @throws IOException 
-	 * @throws MalformedURLException 
-	* 
-	*/	
-			public JSONArray Day5Avg (ArrayList<String> cities) throws NotAllowedValueException, MalformedURLException, IOException, ParseException {
-				JSONArray array = new JSONArray();
-				
-				ArrayList<JSONObject> average = new ArrayList<JSONObject>();
-				ArrayList<Double> averageFeelsLike = new ArrayList<Double>();
-				ArrayList<JSONObject> objects = new ArrayList<JSONObject>();
-				ArrayList<String> names = new ArrayList<String>();
-				
-				Iterator<String> it = cities.iterator();
-				
-				
-				int i = 0;
-				
-				while(it.hasNext()) {
-					JSONObject object = new JSONObject();
-					object = statistic.fiveDayAvg(it.next());
-					average.add(object);
-					double ave = object.getDouble("Real_temp Average");
-					averageFeelsLike.add(ave);
-					
-					JSONObject obj = new JSONObject();
-					obj.put("Name:",cities.get(i));
-					obj.put("real_temp_avg:", ave);
-					objects.add(obj);
-					array.put(obj);
-					
-					i++;
-					
-				}
-				return array;
-				
-			}
-				
-				
+	 * Questo metodo calcola la media delle temperature reali delle città di 5
+	 * giorni
+	 * 
+	 * @param cities contiene le varie città
+	 * @return array con temperatura desiderata
+	 * @throws ParseException        per errore di parsing
+	 * @throws IOException           per errore di input ed output
+	 * @throws MalformedURLException per errore di formazione di URL
+	 */
+
+	public JSONArray Day5Avg(ArrayList<String> cities) throws MalformedURLException, IOException, ParseException {
+		JSONArray array = new JSONArray();
+
+		ArrayList<JSONObject> average = new ArrayList<JSONObject>();
+		ArrayList<Double> averageFeelsLike = new ArrayList<Double>();
+		ArrayList<JSONObject> objects = new ArrayList<JSONObject>();
+		ArrayList<String> names = new ArrayList<String>();
+
+		Iterator<String> it = cities.iterator();
+
+		int i = 0;
+
+		while (it.hasNext()) {
+			JSONObject object = new JSONObject();
+			object = statistic.fiveDayAvg(it.next());
+			average.add(object);
+			double ave = object.getDouble("Real_temp Average");
+			averageFeelsLike.add(ave);
+
+			JSONObject obj = new JSONObject();
+			obj.put("Name:", cities.get(i));
+			obj.put("real_temp_avg:", ave);
+			objects.add(obj);
+			array.put(obj);
+
+			i++;
+
+		}
+		return array;
+
+	}
+
 }
