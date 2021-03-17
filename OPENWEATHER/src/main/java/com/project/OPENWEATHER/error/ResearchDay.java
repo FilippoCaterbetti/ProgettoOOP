4dd013d4fee49982b258bc442ed2faa80d4e0d26
RobertaCantarini <s1095444@studenti.univpm.it>
2021-03-17 18:17:45 +0100

    Update ResearchDay.java

--- OPENWEATHER/src/main/java/com/project/OPENWEATHER/error/ResearchDay.java
+++ OPENWEATHER/src/main/java/com/project/OPENWEATHER/error/ResearchDay.java
@@ -5,70 +5,72 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class ResearchDay {
-	
+
 	public ResearchDay() {
-		
+
 	}
+
 	/**
-	 * Questo metodo viene richiamato dal metodo di Error Calculator per il calcolo dell'errore e si occupa di trovare il 
-	 * giorno presente nello storico da cui partire o terminare (a seconda di quando viene richiamato)
-	 * il calcolo dell'errore.
+	 * Questo metodo viene richiamato dal metodo di Error Calculator per il calcolo
+	 * dell'errore e si occupa di trovare il giorno presente nello storico da cui
+	 * partire o terminare (a seconda di quando viene richiamato) il calcolo
+	 * dell'errore.
 	 * 
-	 * @param CityInfo è il JSONArray che contiene le informazioni generate a partire dall'avvio dello storico, sulle statistiche e sulle date di esse. 
-	 * @param period rappresenta i giorni di predizione, che indicano il giorno da ricercare.
-	 * @return il JSONObject che contiene la data ricercata e la posizione in cui si trova nel JSONArray.
+	 * @param CityInfo è il JSONArray che contiene le informazioni generate a
+	 *                 partire dall'avvio dello storico, sulle statistiche e sulle
+	 *                 date di esse.
+	 * @param period   rappresenta i giorni di predizione, che indicano il giorno da
+	 *                 ricercare.
+	 * @return il JSONObject che contiene la data ricercata e la posizione in cui si
+	 *         trova nel JSONObject.
 	 */
-    public JSONObject researchDay(JSONArray CityInfo, int period) {
-		
+	public JSONObject researchDay(JSONArray CityInfo, int period) {
+
 		String date = "";
-		
+
 		JSONObject Day1 = new JSONObject();
-		Day1 = CityInfo.getJSONObject(0);     //tutte le informazioni della prima ora
+		Day1 = CityInfo.getJSONObject(0); // tutte le informazioni della prima ora
 		date = Day1.getString("data");
-		
-		//firstDate = prende solo il giorno 'dd'
-		String firstDate="";
+
+		// firstDate = prende solo il giorno 'dd'
+		String firstDate = "";
 		firstDate += date.charAt(8);
 		firstDate += date.charAt(9);
-		
-		int i=0;
-		
+
+		int i = 0;
+
 		// periodo da 1 a 5 (inclusi)
-		for(int j = 0; j < period; j++) {
-			
+		for (int j = 0; j < period; j++) {
+
 			boolean control = true;
-			for(i=0; i < CityInfo.length() && control; i++) { //day1.length() = 4 blocchi di statistiche per 24 ore
-			
-				String dates = CityInfo.getJSONObject(i).getString("data");//scorre le date dello storico
-				
+			for (i = 0; i < CityInfo.length() && control; i++) {
+
+				String dates = CityInfo.getJSONObject(i).getString("data");
+
 				String justday = "";
 				justday += dates.charAt(8);
 				justday += dates.charAt(9);
-				
-				if(!justday.equals(firstDate)) {
-					
-					 control = false;		
+
+				if (!justday.equals(firstDate)) {
+
+					control = false;
 				}
-		    }   
-			// i è il valore che fa scattare al giorno successivo
-		
-			
-			date = CityInfo.getJSONObject(i-1).getString("data");
-			
-			firstDate="";
+			}
+
+			date = CityInfo.getJSONObject(i - 1).getString("data");
+
+			firstDate = "";
 			firstDate += date.charAt(8);
 			firstDate += date.charAt(9);
-		
+
 		}
-		
-		i -=1;
+
+		i -= 1;
 		JSONObject info = new JSONObject();
 		info.put("date", CityInfo.getJSONObject(i).getString("data"));
-		info.put("position", i);   //posizione che ha nell'array
-		
-		//System.out.print(info);
+		info.put("position", i); // posizione che ha nell'array
+
 		return info;
-		
+
 	}
 }
-
