e629a929b7cfdd4a04e0c0ddb28c4a3fd595782f
RobertaCantarini <s1095444@studenti.univpm.it>
2021-03-17 18:17:41 +0100

    Update InvalidStringException.java

--- OPENWEATHER/src/main/java/com/project/OPENWEATHER/exception/InvalidStringException.java
+++ OPENWEATHER/src/main/java/com/project/OPENWEATHER/exception/InvalidStringException.java
@@ -2,28 +2,27 @@ package com.project.OPENWEATHER.exception;
 
 import java.lang.Exception;
 
-   /**
-    * errore generato da stringa non regolare
-    *
-    */
-public class InvalidStringException extends Exception{
-	
+/**
+ * errore generato da stringa non regolare
+ *
+ */
+public class InvalidStringException extends Exception {
+
 	String error;
-	
+
 	/**
 	 * @param error è il messaggio di errore.
 	 */
 	public InvalidStringException(String error) {
-		
+
 		this.error = error;
 	}
-	
-	
+
 	/**
 	 * @return String con messaggio di errore
 	 */
 	public String getError() {
-		
+
 		return error;
 	}
 }
