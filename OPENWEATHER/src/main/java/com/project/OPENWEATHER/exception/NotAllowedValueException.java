5c27e5bea986720428d8f6e47e4bace343f0956e
RobertaCantarini <s1095444@studenti.univpm.it>
2021-03-17 18:17:33 +0100

    Update NotAllowedValueException.java

--- OPENWEATHER/src/main/java/com/project/OPENWEATHER/exception/NotAllowedValueException.java
+++ OPENWEATHER/src/main/java/com/project/OPENWEATHER/exception/NotAllowedValueException.java
@@ -1,33 +1,33 @@
 package com.project.OPENWEATHER.exception;
 
 import java.lang.Exception;
+
 /**
  * genera l'eccezione riguardante l'inserimento errato del valore
  *
  */
-public class NotAllowedValueException extends Exception{
+public class NotAllowedValueException extends Exception {
 	String error;
-	
-	
+
 	/**
 	 * @param error rappresenta il messaggio di errore.
 	 */
 	public NotAllowedValueException(String error) {
-		
+
 		this.error = error;
 	}
-	
 
-    /*public NotAllowedValueException(Throwable error) {
-        super(error);
-    }*/
-	
+	/*
+	 * public NotAllowedValueException(Throwable error) { super(error); }
+	 */
+
 	/**
-	 * Restituisce un messaggio di errore 
+	 * Restituisce un messaggio di errore
+	 * 
 	 * @return String che contiene il messaggio d'errore
 	 */
 	public String getError() {
-		
+
 		return error;
 	}
 }
