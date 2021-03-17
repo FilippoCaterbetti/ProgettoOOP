0d71d56435a244401c61e0ab5651d6ae3dd92db8
RobertaCantarini <s1095444@studenti.univpm.it>
2021-03-17 18:17:15 +0100

    Update Filters.java

--- OPENWEATHER/src/main/java/com/project/OPENWEATHER/StatsAndFilters/Filters.java
+++ OPENWEATHER/src/main/java/com/project/OPENWEATHER/StatsAndFilters/Filters.java
@@ -16,108 +16,102 @@ import com.project.OPENWEATHER.exception.NotAllowedValueException;
 import com.project.OPENWEATHER.model.City;
 import com.project.OPENWEATHER.model.Temperature;
 
-public class Filters {    
-	
+public class Filters {
+
 	private ArrayList<String> cities = new ArrayList<String>();
 	private String param;
 	private int period;
-	
+
 	/**
 	 * 
 	 * Costruttore della classe
 	 * 
 	 */
-	
-	public Filters(ArrayList<String> cities, String param, int period) {
-	super();
-	this.cities=cities;
-	this.param=param;
-	this.period=period;
-	}
-		
-	
-	/**
-	 * Questo metodo fa si che a seconda che si scelga 1 o 5 vengano generate le statistiche 
-	 * in un giorno o per 5 giorni
-	 * @return
-	 * @throws NotAllowedPeriodException se non viene inserito il periodo giusto
-	 * @throws ParseException 
-	 * @throws IOException 
-	 * @throws MalformedURLException 
-	 * @throws NotAllowedValueException 	  
-	 */
-	
-	public JSONArray analyze() throws NotAllowedPeriodException, NotAllowedParamException, MalformedURLException, IOException, ParseException, NotAllowedValueException {
-		
-		JSONArray array = new JSONArray ();
-	
-	
-		if(period == 1) {
-			
-				if(param.equals("temp_max")){
-					
-					TempMaxAvg filters = new TempMaxAvg();
-					array = filters.Day1Avg(cities);
 
-				} 
-				else if (param.equals("temp_min")) {
-					
-					TempMinAvg filters = new TempMinAvg();
-					array = filters.Day1Avg(cities);
-					
-				} 
-				else if(param.equals("feels_like")) {
-					
-					FeelsLikeAvg filters = new FeelsLikeAvg();
-					array = filters.Day1Avg(cities);
-					
-				} 
-				else if(param.equals("temp")) {
-					
-					RealTempAvg filters = new RealTempAvg();
-					array = filters.Day1Avg(cities);
-				}
-				else  {
-					
-					throw new NotAllowedParamException (" La stringa " + param + " non è ammessa.Inserisci una stringa tra temp_min,temp_max,feels_like e temp");   
-				}
+	public Filters(ArrayList<String> cities, String param, int period) {
+		super();
+		this.cities = cities;
+		this.param = param;
+		this.period = period;
+	}
+
+	/**
+	 * Questo metodo in base alla scelta effetuata dall'utente, restituisce le
+	 * statistiche di 1 o 5 giorni dei parametri: temp_max, temp_min, temp e
+	 * feels_like
+	 * 
+	 * @return un JSONArray contenente le statistiche
+	 * @throws NotAllowedPeriodException se il periodo inserito non è giusto
+	 * @throws ParseException            per errore di parsing
+	 * @throws IOException               per errore di input ed output
+	 * @throws MalformedURLException     per errore di formazione di URL
+	 */
+
+	public JSONArray analyze() throws NotAllowedPeriodException, NotAllowedParamException, MalformedURLException,
+			IOException, ParseException {
+
+		JSONArray array = new JSONArray();
+
+		if (period == 1) {
+
+			if (param.equals("temp_max")) {
+
+				TempMaxAvg filters = new TempMaxAvg();
+				array = filters.Day1Avg(cities);
+
+			} else if (param.equals("temp_min")) {
+
+				TempMinAvg filters = new TempMinAvg();
+				array = filters.Day1Avg(cities);
+
+			} else if (param.equals("feels_like")) {
+
+				FeelsLikeAvg filters = new FeelsLikeAvg();
+				array = filters.Day1Avg(cities);
+
+			} else if (param.equals("temp")) {
+
+				RealTempAvg filters = new RealTempAvg();
+				array = filters.Day1Avg(cities);
+			} else {
+
+				throw new NotAllowedParamException(" La stringa " + param
+						+ " non è ammessa.Inserisci una stringa tra temp_min,temp_max,feels_like e temp");
 			}
-			
-		else if(period==5) {
-					
-					if(param.equals("temp_max")){
-						
-						TempMaxAvg filters = new TempMaxAvg();
-						array = filters.Day5Avg(cities);
-						
-					} 
-					else if (param.equals("temp_min")) {
-						
-						TempMinAvg filters = new TempMinAvg();
-						array = filters.Day5Avg(cities);
-						
-					} 
-					else if(param.equals("feels_like")) {
-						
-						FeelsLikeAvg filters = new FeelsLikeAvg();
-						array = filters.Day5Avg(cities);
-						
-					} 
-					else if(param.equals("temp")) {
-						
-						RealTempAvg filters = new RealTempAvg();
-						array = filters.Day5Avg(cities);
-					}
-					else {
-						
-						throw new NotAllowedParamException (param + " non è una stringa ammessa.Inserisci una stringa tra temp_min,temp_max,feels_like e temp");
-					}
-				}
-		else {
-			
-			throw new NotAllowedPeriodException (period + " non è un numero ammesso. Inserisci un numero che sia o 1 o 5.");
 		}
-		
+
+		else if (period == 5) {
+
+			if (param.equals("temp_max")) {
+
+				TempMaxAvg filters = new TempMaxAvg();
+				array = filters.Day5Avg(cities);
+
+			} else if (param.equals("temp_min")) {
+
+				TempMinAvg filters = new TempMinAvg();
+				array = filters.Day5Avg(cities);
+
+			} else if (param.equals("feels_like")) {
+
+				FeelsLikeAvg filters = new FeelsLikeAvg();
+				array = filters.Day5Avg(cities);
+
+			} else if (param.equals("temp")) {
+
+				RealTempAvg filters = new RealTempAvg();
+				array = filters.Day5Avg(cities);
+			} else {
+
+				throw new NotAllowedParamException(param
+						+ " non è una stringa ammessa.Inserisci una stringa tra temp_min,temp_max,feels_like e temp");
+			}
+		} else {
+
+			throw new NotAllowedPeriodException(
+					period + " non è un numero ammesso. Inserisci un numero che sia o 1 o 5.");
+		}
+
 		return array;
 	}
 }
