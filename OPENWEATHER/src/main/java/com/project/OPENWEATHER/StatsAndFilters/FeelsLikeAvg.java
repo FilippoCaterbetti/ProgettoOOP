90b6f801579faaac49722559a6d6edab4411d840
RobertaCantarini <s1095444@studenti.univpm.it>
2021-03-17 18:17:18 +0100

    Update FeelsLikeAvg.java

--- OPENWEATHER/src/main/java/com/project/OPENWEATHER/StatsAndFilters/FeelsLikeAvg.java
+++ OPENWEATHER/src/main/java/com/project/OPENWEATHER/StatsAndFilters/FeelsLikeAvg.java
@@ -15,100 +15,97 @@ import com.project.OPENWEATHER.exception.NotAllowedValueException;
 import com.project.OPENWEATHER.model.City;
 import com.project.OPENWEATHER.model.Temperature;
 
-public class FeelsLikeAvg implements FiltersStatistics{
-			
-	Statistics statistic = new Statistics();
-/**
- * 
- * Questo metodo calcola la media delle 
- * temperature percepita delle città in un giorno 
- * @throws ParseException 
- * @throws IOException 
- * @throws MalformedURLException 
- * 
- */
-	public JSONArray Day1Avg (ArrayList<String> cities) throws NotAllowedValueException, MalformedURLException, IOException, ParseException{
+public class FeelsLikeAvg implements FiltersStatistics {
 
-        JSONArray array = new JSONArray();
-		
+	Statistics statistic = new Statistics();
+
+	/**
+	 * Questo metodo calcola la media delle temperature percepite delle città di 1
+	 * giorno
+	 * 
+	 * @param cities contiene le varie città
+	 * @return array con temperatura desiderata
+	 * @throws ParseException        per errore di parsing
+	 * @throws IOException           per errore di input ed output
+	 * @throws MalformedURLException per errore di formazione di URL
+	 */
+	public JSONArray Day1Avg(ArrayList<String> cities) throws MalformedURLException, IOException, ParseException {
+
+		JSONArray array = new JSONArray();
+
 		ArrayList<JSONObject> avg = new ArrayList<JSONObject>();
 		ArrayList<Double> avgFeelsLike = new ArrayList<Double>();
 		ArrayList<JSONObject> objects = new ArrayList<JSONObject>();
 		ArrayList<String> names = new ArrayList<String>();
-		
+
 		Iterator<String> it = cities.iterator();
-		
-	
+
 		int i = 0;
-		
-		while(it.hasNext()) {
-			
+
+		while (it.hasNext()) {
+
 			JSONObject object = new JSONObject();
 			object = statistic.todayAvg(it.next());
 			avg.add(object);
 			double ave = object.getDouble("Feels_like Average");
 			avgFeelsLike.add(ave);
-			
+
 			JSONObject obj = new JSONObject();
-			obj.put("Name:", cities.get(i)); 
-			obj.put("feels_like_average:",ave);
+			obj.put("Name:", cities.get(i));
+			obj.put("feels_like_average:", ave);
 			objects.add(obj);
 			array.put(obj);
-			
+
 			i++;
-			
+
 		}
-		
-		
+
 		return array;
 
 	}
-	
-/**
-* 
-* Questo metodo calcola la media delle 
-* temperature percepita delle città in 5 giorno 
- * @throws ParseException 
- * @throws IOException 
- * @throws MalformedURLException 
-* 
-*/
-	
-	public JSONArray Day5Avg (ArrayList<String> cities) throws NotAllowedValueException, MalformedURLException, IOException, ParseException {
+
+	/**
+	 * 
+	 * Questo metodo calcola la media delle temperature percepite delle città di 5
+	 * giorni
+	 * 
+	 * @param cities contiene le varie città
+	 * @return array con temperatura desiderata
+	 * @throws ParseException        per errore di parsing
+	 * @throws IOException           per errore di input ed output
+	 * @throws MalformedURLException per errore di formazione di URL
+	 */
+
+	public JSONArray Day5Avg(ArrayList<String> cities) throws MalformedURLException, IOException, ParseException {
 		JSONArray array = new JSONArray();
-		
+
 		ArrayList<JSONObject> average = new ArrayList<JSONObject>();
 		ArrayList<Double> averageFeelsLike = new ArrayList<Double>();
 		ArrayList<JSONObject> objects = new ArrayList<JSONObject>();
 		ArrayList<String> names = new ArrayList<String>();
-		
+
 		Iterator<String> it = cities.iterator();
-	
-		
+
 		int i = 0;
-		
-		while(it.hasNext()) {
-			
+
+		while (it.hasNext()) {
+
 			JSONObject object = new JSONObject();
 			object = statistic.fiveDayAvg(it.next());
 			average.add(object);
 			double ave = object.getDouble("Feels_like Average");
 			averageFeelsLike.add(ave);
-			
+
 			JSONObject obj = new JSONObject();
-			obj.put("Name:",cities.get(i));
-			obj.put("feels_like_avg:",ave);
+			obj.put("Name:", cities.get(i));
+			obj.put("feels_like_avg:", ave);
 			objects.add(obj);
 			array.put(obj);
-			
+
 			i++;
-			
+
 		}
 		return array;
-		
-	}		
-}	
 
-
-
-	
+	}
+}
