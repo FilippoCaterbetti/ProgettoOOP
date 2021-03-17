9bf8d56c6d325d541f9dc8a97d65e6240585c43c
RobertaCantarini <s1095444@studenti.univpm.it>
2021-03-17 18:17:36 +0100

    Update NotAllowedParamException.java

--- OPENWEATHER/src/main/java/com/project/OPENWEATHER/exception/NotAllowedParamException.java
+++ OPENWEATHER/src/main/java/com/project/OPENWEATHER/exception/NotAllowedParamException.java
@@ -1,28 +1,29 @@
 package com.project.OPENWEATHER.exception;
+
 import java.lang.Exception;
 
-  /**
-   * generiamo l'eccezione a causa di una stringa non ammessa per param
-   *
-   */
-public class NotAllowedParamException extends Exception{
-	
+/**
+ * generiamo l'eccezione a causa di una stringa non ammessa per param
+ *
+ */
+public class NotAllowedParamException extends Exception {
+
 	String error;
-	
+
 	/**
 	 * @param error è il messaggio di errore.
 	 */
 	public NotAllowedParamException(String error) {
-		
+
 		super(error);
 		this.error = error;
 	}
-	
+
 	/**
 	 * @return String con messaggio di errore
 	 */
 	public String getError() {
-		
+
 		return error;
 	}
 }
