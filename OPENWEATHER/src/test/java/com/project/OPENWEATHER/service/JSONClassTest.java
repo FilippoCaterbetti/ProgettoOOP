c6a7a0257068a828309a3a79bef38c7b5d19286d
RobertaCantarini <s1095444@studenti.univpm.it>
2021-03-17 18:16:55 +0100

    Update JSONClassTest.java

--- OPENWEATHER/src/test/java/com/project/OPENWEATHER/service/JSONClassTest.java
+++ OPENWEATHER/src/test/java/com/project/OPENWEATHER/service/JSONClassTest.java
@@ -17,16 +17,16 @@ import com.project.OPENWEATHER.model.City;
 import com.project.OPENWEATHER.model.Temperature;
 
 public class JSONClassTest {
-	
-	
+
 	City city;
 	Vector<Temperature> temp;
 	JSONClass tojson;
-	
+
 	/**
-     * Inizializza i componenti necessari a testare i metodi.
-     * @throws java.lang.Exception
-     */
+	 * Inizializza i componenti necessari a testare i metodi.
+	 * 
+	 * @throws java.lang.Exception
+	 */
 	@BeforeEach
 	void setUp() throws Exception {
 		city = new City();
@@ -35,37 +35,37 @@ public class JSONClassTest {
 	}
 
 	/**
-     * Serve per distruggere ciò che è stato inizializzato dal metodo setUp.
-     * throws java.lang.Exception
-     */
+	 * Serve per distruggere ciò che è stato inizializzato dal metodo setUp.
+	 * @throws java.lang.Exception
+	 */
 	@AfterEach
 	void tearDown() throws Exception {
 	}
-	
+
 	/**
-     * Questo Test verifica se l'oggetto city viene converito correttamente in JSONObject.
-     */
+	 * Questo Test verifica se l'oggetto city viene convertito correttamente in
+	 * JSONObject.
+	 */
 	@Test
-    @DisplayName("Conversione effettuata")
-    void toJson() throws IOException {
-    	
-        city.setName("Ancona");
-        city.setId(42);
-        city.setCountry("IT");
-        Temperature temps = new Temperature(25,30,40,20,30,"15-03-2021");
-        temp.add(temps);
-        city.setTemps(temp);
-        
-        JSONObject obj = new JSONObject();
-        
-        obj.put("name", city.getName());
-        obj.put("country", city.getCountry());
+	@DisplayName("Conversione effettuata")
+	void toJson() throws IOException {
+
+		city.setName("Ancona");
+		city.setId(42);
+		city.setCountry("IT");
+		Temperature temps = new Temperature(25, 30, 40, 20, 30, "15-03-2021");
+		temp.add(temps);
+		city.setTemps(temp);
+
+		JSONObject obj = new JSONObject();
+
+		obj.put("name", city.getName());
+		obj.put("country", city.getCountry());
 		obj.put("id", city.getId());
-        
+
 		JSONArray tempArr = new JSONArray();
-		
+
 		JSONObject tempObj = new JSONObject();
-	
 
 		tempObj.put("temp", (temps.getTemp()));
 		tempObj.put("feels_like", (temps.getFeels_like()));
@@ -74,14 +74,13 @@ public class JSONClassTest {
 		tempObj.put("temp_avg", (temps.getTemp_avg()));
 
 		tempObj.put("data", (temps.getData()));
-		
-		tempArr.put(tempObj);
-		
-		obj.put("OpenWeather", tempArr);
-		
-		assertEquals(obj.toString(),tojson.JSONClass(city).toString());
-		
-    }
 
-	
+		tempArr.put(tempObj);
+
+		obj.put("OpenWeather", tempArr);
+
+		assertEquals(obj.toString(), tojson.JSONClass(city).toString());
+
+	}
+
 }
