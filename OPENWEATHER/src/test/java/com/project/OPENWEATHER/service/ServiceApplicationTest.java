0da9d01ae5ec08105be3a3d6362beb54ad8f7791
RobertaCantarini <s1095444@studenti.univpm.it>
2021-03-17 18:16:53 +0100

    Update ServiceApplicationTest.java

--- OPENWEATHER/src/test/java/com/project/OPENWEATHER/service/ServiceApplicationTest.java
+++ OPENWEATHER/src/test/java/com/project/OPENWEATHER/service/ServiceApplicationTest.java
@@ -1,4 +1,5 @@
 package com.project.OPENWEATHER.service;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.IOException;
@@ -17,84 +18,84 @@ import org.junit.jupiter.api.Test;
 import com.project.OPENWEATHER.exception.CitynotFoundException;
 import com.project.OPENWEATHER.exception.EmptyStringException;
 
-
 public class ServiceApplicationTest {
 
 	private ServiceApplication service;
-    private ArrayList<String> names;
-    
-	 /**
-     * Inizializza i componenti necessari a testare i metodi.
-     * @throws java.lang.Exception
-     */
+	private ArrayList<String> names;
+
+	/**
+	 * Inizializza i componenti necessari a testare i metodi.
+	 * 
+	 * @throws java.lang.Exception
+	 */
 	@BeforeEach
 	void setUp() throws Exception {
 		service = new ServiceApplication();
 		names = new ArrayList<String>();
-		}
+	}
 
 	/**
-     * Serve per distruggere ciò che è stato inizializzato dal metodo setUp.
-     * throws java.lang.Exception
-     */
+	 * Serve per distruggere ciò che è stato inizializzato dal metodo setUp.
+	 * 
+	 * @throws java.lang.Exception
+	 */
 	@AfterEach
 	void tearDown() throws Exception {
 	}
-	
+
 	/**
-     * Questo Test verifica se il path in cui viene salvato il file è corretto.
-	 * @throws ParseException 
-     */
+	 * Questo Test verifica se il path in cui viene salvato il file è corretto.
+	 * 
+	 * @throws ParseException
+	 */
 	@Test
-    @DisplayName("Salvataggio corretto del file")
-    void salvaFile() throws IOException, ParseException {
-    	
-        String name = "Ancona";
-        
-        SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd");
+	@DisplayName("Salvataggio corretto del file")
+	void salvaFile() throws IOException, ParseException {
+
+		String name = "Ancona";
+
+		SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd");
 		String today = date.format(new Date());
-		
-		String path = System.getProperty("user.dir") +"\\"+name+"_"+today+ ".txt";
-		
-        assertEquals(path,service.save(name));
-        
-        
-    }
-    
 
+		String path = System.getProperty("user.dir") + "\\" + name + "_" + today + ".txt";
 
-	
-	// Questo Test verifica se viene generata correttamente l'eccezione CityNotFound.
-	 
-    @Test
-    @DisplayName("Corretta generazione dell'eccezione CityNotFoundException.")
-    void readHistory1() throws ParseException{
-		
-    	names.add("Bologna");
-        names.add("Mantova");
-    	
-        CitynotFoundException e = assertThrows(CitynotFoundException.class, () -> {service.HistoryOfError(names,1,"max",1);});
-    
-        assertEquals("La città inserita non è presente nello storico", e.getError());
-        
-    }
-    
+		assertEquals(path, service.save(name));
 
-	
-	//Questo Test verifica se viene generata correttamente l'eccezione EmptyString.
-	 
+	}
+
+	// Questo Test verifica se viene generata correttamente l'eccezione
+	// CityNotFoundException.
+
 	@Test
-    @DisplayName("Corretta generazione dell'eccezione EmptyStringException.")
-    void readHistory2() {
-	
-    	names.add("Ancona");
-        names.add("");
-    	
-        EmptyStringException e = assertThrows(EmptyStringException.class, () -> {service.HistoryOfError(names,1,"max",1);});
-        
-        assertEquals("Hai dimenticato di inserire la città", e.getError());
-       
-    }
-	
-	
+	@DisplayName("Corretta generazione dell'eccezione CityNotFoundException.")
+	void readHistory1() throws ParseException {
+
+		names.add("Bologna");
+		names.add("Mantova");
+
+		CitynotFoundException e = assertThrows(CitynotFoundException.class, () -> {
+			service.HistoryOfError(names, 1, "max", 1);
+		});
+
+		assertEquals("La città inserita non è presente nello storico", e.getError());
+
+	}
+
+	// Questo Test verifica se viene generata correttamente l'eccezione EmptyStringException.
+
+	@Test
+	@DisplayName("Corretta generazione dell'eccezione EmptyStringException.")
+	void readHistory2() {
+
+		names.add("Ancona");
+		names.add("");
+
+		EmptyStringException e = assertThrows(EmptyStringException.class, () -> {
+			service.HistoryOfError(names, 1, "max", 1);
+		});
+
+		assertEquals("Hai dimenticato di inserire la città", e.getError());
+
+	}
+
 }
