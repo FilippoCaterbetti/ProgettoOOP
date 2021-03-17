2d778b033a32619fc4a4d23ae02cba0bd4627a7b
RobertaCantarini <s1095444@studenti.univpm.it>
2021-03-17 18:17:04 +0100

    Update TempMinAvg.java

--- OPENWEATHER/src/main/java/com/project/OPENWEATHER/StatsAndFilters/TempMinAvg.java
+++ OPENWEATHER/src/main/java/com/project/OPENWEATHER/StatsAndFilters/TempMinAvg.java
@@ -15,95 +15,95 @@ import com.project.OPENWEATHER.exception.NotAllowedValueException;
 import com.project.OPENWEATHER.model.City;
 import com.project.OPENWEATHER.model.Temperature;
 
-public class TempMinAvg implements FiltersStatistics{
+public class TempMinAvg implements FiltersStatistics {
 
-		Statistics statistic = new Statistics();
+	Statistics statistic = new Statistics();
+
 	/**
-	* 
-	* @param cities contiene le varie città
-	* @return array con temperatura desiderata 
-	* @throws NotAllowedValueException
-	* @throws ParseException  per errore di parsing
-	* @throws IOException  per errore di input ed output
-	* @throws MalformedURLException per errore di formazione di URL
-	*/
+	 * Questo metodo calcola la media delle temperature minime delle città di 1
+	 * giorno
+	 * 
+	 * @param cities contiene le varie città
+	 * @return array con temperatura desiderata
+	 * @throws ParseException        per errore di parsing
+	 * @throws IOException           per errore di input ed output
+	 * @throws MalformedURLException per errore di formazione di URL
+	 */
 
-		public JSONArray Day1Avg (ArrayList<String> cities) throws NotAllowedValueException, MalformedURLException, IOException, ParseException {
+	public JSONArray Day1Avg(ArrayList<String> cities) throws MalformedURLException, IOException, ParseException {
 
-			JSONArray array = new JSONArray();
-						
-				ArrayList<JSONObject> avg = new ArrayList<JSONObject>();
-				ArrayList<Double> avgMinTemp = new ArrayList<Double>();
-				ArrayList<JSONObject> objects = new ArrayList<JSONObject>();
-				ArrayList<String> names = new ArrayList<String>();
-						
-						Iterator<String> it = cities.iterator();
-						
-						
-						int i = 0;
-						
-						while(it.hasNext()) {
-							JSONObject object = new JSONObject();
-							object = statistic.todayAvg(it.next());
-							avg.add(object);
-							double ave = object.getDouble("Temp_Min Average");
-							avgMinTemp.add(ave);
-							
-							JSONObject obj = new JSONObject();
-							obj.put("Name:", cities.get(i)); 
-							obj.put("temp_min_avg:",ave);
-							objects.add(obj);
-							array.put(obj);
-							
-							i++;
-						}
-						
-						return array;
+		JSONArray array = new JSONArray();
 
-					}
-						
-		/**
-		* 
-		* Questo metodo calcola la media delle 
-		* temperature percepita delle città in 5 giorno 
-		* @param cities contiene le varie città
-		* @return array con temperatura desiderata 
-		* @throws ParseException  per errore di parsing
-		* @throws IOException  per errore di input ed output
-		* @throws MalformedURLException per errore di formazione di URL
-		* 
-		*/		
-			public JSONArray Day5Avg (ArrayList<String> cities) throws NotAllowedValueException, MalformedURLException, IOException, ParseException {
-						JSONArray array = new JSONArray();
-						
-						ArrayList<JSONObject> average = new ArrayList<JSONObject>();
-						ArrayList<Double> avgMinTemp = new ArrayList<Double>();
-						ArrayList<JSONObject> objects = new ArrayList<JSONObject>();
-						ArrayList<String> names = new ArrayList<String>();
-						
-						Iterator<String> it = cities.iterator();
-						
-						
-						int i = 0;
-						
-						while(it.hasNext()) {
-							
-							JSONObject object = new JSONObject();
-							object = statistic.fiveDayAvg(it.next());
-							average.add(object);
-							double ave = object.getDouble("Temp_Min Average");
-							avgMinTemp.add(ave);
-							
-							JSONObject obj = new JSONObject();
-							obj.put("Name:",cities.get(i));
-							obj.put("temp_min_avg:",ave);
-							objects.add(obj);
-							array.put(obj);
-							
-							i++;
-							
-						}
-						
-						return array;
-			}
+		ArrayList<JSONObject> avg = new ArrayList<JSONObject>();
+		ArrayList<Double> avgMinTemp = new ArrayList<Double>();
+		ArrayList<JSONObject> objects = new ArrayList<JSONObject>();
+		ArrayList<String> names = new ArrayList<String>();
+
+		Iterator<String> it = cities.iterator();
+
+		int i = 0;
+
+		while (it.hasNext()) {
+			JSONObject object = new JSONObject();
+			object = statistic.todayAvg(it.next());
+			avg.add(object);
+			double ave = object.getDouble("Temp_Min Average");
+			avgMinTemp.add(ave);
+
+			JSONObject obj = new JSONObject();
+			obj.put("Name:", cities.get(i));
+			obj.put("temp_min_avg:", ave);
+			objects.add(obj);
+			array.put(obj);
+
+			i++;
+		}
+
+		return array;
+
+	}
+
+	/**
+	 * Questo metodo calcola la media delle temperature minime delle città di 5
+	 * giorni
+	 * 
+	 * @param cities contiene le varie città
+	 * @return array con temperatura desiderata
+	 * @throws ParseException        per errore di parsing
+	 * @throws IOException           per errore di input ed output
+	 * @throws MalformedURLException per errore di formazione di URL
+	 */
+
+	public JSONArray Day5Avg(ArrayList<String> cities) throws MalformedURLException, IOException, ParseException {
+		JSONArray array = new JSONArray();
+
+		ArrayList<JSONObject> average = new ArrayList<JSONObject>();
+		ArrayList<Double> avgMinTemp = new ArrayList<Double>();
+		ArrayList<JSONObject> objects = new ArrayList<JSONObject>();
+		ArrayList<String> names = new ArrayList<String>();
+
+		Iterator<String> it = cities.iterator();
+
+		int i = 0;
+
+		while (it.hasNext()) {
+
+			JSONObject object = new JSONObject();
+			object = statistic.fiveDayAvg(it.next());
+			average.add(object);
+			double ave = object.getDouble("Temp_Min Average");
+			avgMinTemp.add(ave);
+
+			JSONObject obj = new JSONObject();
+			obj.put("Name:", cities.get(i));
+			obj.put("temp_min_avg:", ave);
+			objects.add(obj);
+			array.put(obj);
+
+			i++;
+
+		}
+
+		return array;
+	}
 }
