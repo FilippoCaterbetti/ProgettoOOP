8f246aeaec23325e26c5051dee53245101258e43
RobertaCantarini <s1095444@studenti.univpm.it>
2021-03-17 18:17:00 +0100

    Update TestCity.java

--- OPENWEATHER/src/test/java/com/project/OPENWEATHER/Model/TestCity.java
+++ OPENWEATHER/src/test/java/com/project/OPENWEATHER/Model/TestCity.java
@@ -2,7 +2,6 @@ package com.project.OPENWEATHER.Model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,25 +10,37 @@ import com.project.OPENWEATHER.model.City;
 
 class TestCity {
 
-		private City citta1;
-		private City citta2;
-		
-		@BeforeEach
-		void setUp() throws Exception {
-			citta1 = new City("Milan","Italy",1);
-			citta2 = new City("Roma","Italy",2);
-		}
+	private City citta1;
+	private City citta2;
 
-		@AfterEach
-		void tearDown() throws Exception {
-		}
+	/**
+	 * Inizializza i componenti necessari a testare i metodi.
+	 * 
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+		citta1 = new City("Milan", "Italy", 1);
+		citta2 = new City("Roma", "Italy", 2);
+	}
 
-		@Test
-		void testToJSONObject() {
-			assertEquals("Milan", citta1.toJSONObject().get("name"));
-			assertEquals("Italy", citta1.toJSONObject().get("country"));
-			assertEquals(1L, citta1.toJSONObject().get("id"));
-			}
-		
+	/**
+	 * Serve per distruggere ciò che è stato inizializzato dal metodo setUp.
+	 * 
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	/*
+	 * serve per controllare che i parametri passati siano giusti
+	 */
+	@Test
+	void testToJSONObject() {
+		assertEquals("Milan", citta1.toJSONObject().get("name"));
+		assertEquals("Italy", citta1.toJSONObject().get("country"));
+		assertEquals(1L, citta1.toJSONObject().get("id"));
+	}
+
 }
-
